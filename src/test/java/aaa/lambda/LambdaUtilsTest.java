f3ec5af3ac4b923b43385bf529ac6f18807149bd
Anton Akkuzin <akkuzin@gmail.com>
2016-05-09 21:21:57 +0300

    removed redundant

--- src/test/java/aaa/lambda/LambdaUtilsTest.java
+++ src/test/java/aaa/lambda/LambdaUtilsTest.java
@@ -1,11 +1,7 @@
 package aaa.lambda;
 
-import aaa.nvl.Nvl;
 import org.junit.Test;
 
-import java.util.List;
-
-import static aaa.lambda.LambdaUtils.comparator;
 import static aaa.lambda.LambdaUtils.range;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
@@ -18,19 +14,6 @@ public class LambdaUtilsTest {
 		assertEquals(asList(1, 2, 3, 4, 5), range(1, 6).boxed().collect(toList()));
 		assertEquals(asList(2, 1, 0, -1, -2), range(2, -3).boxed().collect(toList()));
 		assertEquals(asList(-5, -4, -3, -2, -1, 0), range(-5, 1).boxed().collect(toList()));
-	}
-
-	@Test
-	public void comparatorTest() {
-		assertEquals(	asList(asList("info"), asList("some text"), asList("zzz")),
-						asList(asList("info"), asList("zzz"), asList("some text")).stream()
-								.sorted(comparator(Nvl::nvlGetFirst))
-								.collect(toList()));
-		assertEquals(	asList(asList("zzz"), asList("some text"), asList("info")),
-						asList(asList("info"), asList("zzz"), asList("some text")).stream()
-								.sorted(LambdaUtils.<List<String>, String> comparator(Nvl::nvlGetFirst)
-										.reversed())
-								.collect(toList()));
 	}
 
 }
